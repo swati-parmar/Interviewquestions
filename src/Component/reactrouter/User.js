65fc1892ed853a4688216b5c45c33d82429abe7a
Your Name <you@example.com>
2023-02-15 23:13:17 +0530

    use useHistory Hook

--- src/Component/reactrouter/User.js
+++ src/Component/reactrouter/User.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
 
 // const User = ({ match }) => {
 //     return  <h1>Hello {match.params.name} page </h1>
@@ -8,14 +8,14 @@ import { useParams, useLocation } from 'react-router-dom';
 const User = () => {
     const { firstname, lastname } = useParams();
     const location = useLocation();
-    console.log(location);
+    const navigate = useNavigate();
 
     return  (
         <div>
             <h1>Hello {firstname} {lastname} page </h1>
             <p>My current location is {location.pathname} </p>
             {location.pathname === `/user/swati/parmar` ? (
-            <button onClick={() => alert(`you are awesome`)}> Click Me</button>
+            <button onClick={() => navigate(-1)}> Go back</button>
             ) : null}
         </div>
     )
